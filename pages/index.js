114518d55a26dadddd3c6091d71ac2ffacd27529
hasegawa2073 <hasegawatatsuya206@gmail.com>
2022-11-23 15:51:28 +0900

    add: 投稿内容を静的生成

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,38 @@
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Layout from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
-    <h1 className="title">
-      Read<Link href="/posts/first-post">this page!</Link>
-    </h1>
+    <Layout home>
+      <h1 className="title">
+        Read<Link href="/posts/first-post">this page!</Link>
+      </h1>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
